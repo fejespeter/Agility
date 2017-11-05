fd54cb7cc12d46f34a7c62e385e6c7c0473be01a
fejespeter <fejespeter11@gmail.com>
2017-11-05 21:54:22 +0100

    init repository

---
+++ SmartGrass/SensorData.java
@@ -0,0 +1,39 @@
+package smartgrass;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gulya
+ */
+class SensorData{
+    private VitaminState vitaminState;
+    private MoistureState moistureState;
+    
+    public SensorData(){
+        
+    }
+    public SensorData(VitaminState vs, MoistureState ms){
+        vitaminState=vs;
+        moistureState=ms;
+    }
+    
+    public void setVitaminState(VitaminState vs){
+        vitaminState=vs;
+    }
+    public void setMoistureState(MoistureState ms){
+        moistureState=ms;
+    }
+    
+    public VitaminState getVitaminState(){
+        return vitaminState;
+    }
+    public MoistureState getMoistureState(){
+        return moistureState;
+    }
+
+}
